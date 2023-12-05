a680876a8d0200f969a93a9aaa85e258958c084d
rasmusq <rasmus.qku@gmail.com>
2023-12-05 14:13:13 +0100

    Reintroduce keybinds for escape

--- nvim/lua/keybinds.lua
+++ nvim/lua/keybinds.lua
@@ -1,24 +1,27 @@
 -- Keybindings
 vim.g.mapleader = " "
+vim.api.nvim_set_keymap("c", "mg", "<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "mg", "<esc>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "mg", "<esc>", { noremap = true, silent = true })
 
 -- Make indenting in visual mode keep the selection
 vim.api.nvim_set_keymap("v", "<", "<gv", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("v", ">", ">gv", { noremap = true, silent = true })
 
 -- Paste from system clipboard
-vim.keymap.set("n", "<leader>p", '"+p', { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>P", '"+P', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>p", '"+p', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>P", '"+P', { noremap = true, silent = true })
 
-vim.keymap.set("n", "<leader>y", '"+y', { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>Y", '"+Y', { noremap = true, silent = true })
-vim.keymap.set("v", "<leader>y", '"+y', { noremap = true, silent = true })
-vim.keymap.set("v", "<leader>Y", '"+Y', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>y", '"+y', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>Y", '"+Y', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<leader>y", '"+y', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<leader>Y", '"+Y', { noremap = true, silent = true })
 
 -- Redo
-vim.keymap.set("n", "U", "<cmd>redo<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "U", "<cmd>redo<cr>", { noremap = true, silent = true })
 
 -- Escape Terminal
-vim.keymap.set("t", "<esc>", "<C-\\><C-n>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("t", "<esc>", "<C-\\><C-n>", { noremap = true, silent = true })
 
 -- Open Terminal
 vim.api.nvim_set_keymap("n", "<leader>Q", "<cmd>!blackbox -w $PWD<cr>", { noremap = true, silent = true })
